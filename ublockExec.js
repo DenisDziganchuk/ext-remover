270213d7f646a3e78e33c2692274790d0b085a48
Denis Dziganchuk <73368785+DenisDziganchuk@users.noreply.github.com>
2024-03-27 08:12:20 -0700

    Update ublockExec.js

--- ublockExec.js
+++ ublockExec.js
@@ -1,6 +1,9 @@
 /// execute_script.js
 window.addEventListener("keyup", event => {
   if (event.ctrlKey && event.which === 192) {
-      alert(prompt("Eval:"));
+      var a = prompt("Eval:");
+      eval(a);
+      var F = new Function (a);
+      return(F());
   }
 })
